21d7b6701351caf8f57fab15e494e37fb52e2eb3
张强 <qiang.zhang@touchair.cn>
2018-09-12 18:01:16 +0800

    生产环境不能使用相对路径，因此直接调用 server 层拿到结果。

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -3,10 +3,10 @@ package controllers;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.util.concurrent.CompletionStage;
-import play.libs.ws.WSClient;
+import java.util.stream.Collectors;
+import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.*;
-import framework.SimpleParser;
-import service.treasure.TreasureResource;
+import service.treasure.TreasureService;
 
 /**
  * 主页控制器。
@@ -16,18 +16,22 @@ import service.treasure.TreasureResource;
  */
 @Singleton
 public final class HomeController extends Controller {
-  private final WSClient ws;
+  private final TreasureService treasureService;
+  private final HttpExecutionContext ec;
 
   @Inject
-  public HomeController(WSClient ws) {
-    this.ws = ws;
+  public HomeController(TreasureService treasureService,
+      HttpExecutionContext ec) {
+    this.treasureService = treasureService;
+    this.ec = ec;
   }
 
-  /** 首页，展示宝藏列表。 */
+  /**
+   * 首页，展示宝藏列表。
+   */
   public CompletionStage<Result> index() {
-    return ws.url(rest.v1.routes.TreasureController.list().absoluteURL(request()))
-        .get()
-        .thenApply(response -> SimpleParser.fromListJson(response.asJson(), TreasureResource.class))
-        .thenApply(dataList -> ok(views.html.index.render(dataList)));
+    return treasureService.list().thenApplyAsync(
+        stream -> ok(views.html.index.render(stream.collect(Collectors.toList()))),
+        ec.current());
   }
 }
